23bd8b68925249408095c6df3fb9705504a6a36e
francisco souza <fsouza@users.noreply.github.com>
2020-10-16 13:08:44 -0400

    Não pode banir o Chico

--- commands/ban.js
+++ commands/ban.js
@@ -14,12 +14,14 @@ exports.default = (client, target, context, message) => {
       );
     }
 
+    const tentouBanirOChico = splittedMessage[1].toLowerCase() === '@chicocodes';
     if (splittedMessage.length >= 2 && splittedMessage[1][0] === '@') {
-      if (randomBan > 100 && randomBan < 1000) {
+      if (randomBan > 100 && randomBan < 1000 && !tentouBanirOChico) {
         client.say(target, `/timeout ${splittedMessage[1]} 10`);
         client.say(target, `/me ${splittedMessage[1]} foi pego pelo panda do mal.`);
-      } else if (randomBan > 1000 && randomBan < 7000) {
-        client.say(target, `/timeout ${context.username} 10`);
+      } else if (randomBan > 1000 && randomBan < 7000 || tentouBanirOChico) {
+        const timeoutDuration = tentouBanirOChico ? 1000000 : 10;
+        client.say(target, `/timeout ${context.username} ${timeoutDuration}`);
         client.say(target, `${context.username} foi pego pelo panda do mal.`);
       } else {
         client.say(target, `Todos escaparam do panda do mal. Grrrr`);
