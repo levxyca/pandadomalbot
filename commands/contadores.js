9157439ab7d319dc4cc5c92cf2b84f640ab37be8
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2020-10-18 18:28:19 -0300

    Merge com upstream

--- commands/contadores.js
+++ commands/contadores.js
@@ -1,62 +1,69 @@
-const { salvarDados } = require('../utils')
+const { salvarDados } = require('../utils');
 
 const COMANDOS_CONTADORES = {
-  '!calma' : {
+  '!calma': {
     palavra: 'CALMA',
     contadorTotal: 'qtdCalma',
     contadorDiario: 'qtdCalmaH',
-    temCombo: true
+    temCombo: true,
   },
-  '!eita' : {
+  '!eita': {
     palavra: 'EITA',
     contadorTotal: 'qtdEita',
     contadorDiario: 'qtdEitaH',
-    temCombo: true
+    temCombo: true,
   },
   '!oh': {
     palavra: 'Ó',
     contadorTotal: 'qtdOh',
     contadorDiario: 'qtdOhH',
-    temCombo: true
-  }
-}
+    temCombo: true,
+  },
+};
 
 const DATA_INICIAL = '09/10/2020';
 const COMBO_MAX = 20;
 
 const isComboValido = (combo) => {
-  return combo && !isNaN(combo) && combo > 0 && combo < COMBO_MAX;
-}
+  return (
+    combo && !Number.isNaN(Number(combo)) && combo > 0 && combo < COMBO_MAX
+  );
+};
 
 exports.default = (client, target, context, message, dados) => {
-    if (!dados) {
-      console.log('Objeto de dados vazio');
-      return;
-    }
+  if (!dados) return;
 
-    const data = [new Date().getDate(), new Date().getMonth() + 1, new Date().getFullYear()].join('/')
-    
-    const commandName = message.trim();
-    const [comando, combo] = commandName.split(' ');
-    const comandoContador = COMANDOS_CONTADORES[comando]
+  const data = [
+    new Date().getDate(),
+    new Date().getMonth() + 1,
+    new Date().getFullYear(),
+  ].join('/');
 
-    if (!comandoContador) return;
+  const commandName = message.trim();
+  const [comando, combo] = commandName.split(' ');
+  const comandoContador = COMANDOS_CONTADORES[comando];
 
-    let contadorTotal = dados.table[comandoContador.contadorTotal] || 0;
-    let contadorDiario = dados.table.ultimaData === data ? (dados.table[comandoContador.contadorDiario] || 0) : 0;
+  if (!comandoContador) return;
 
-    const valorASerSomado = comandoContador.temCombo && isComboValido(combo) ? parseInt(combo) : 1; 
-    contadorTotal += valorASerSomado;
-    contadorDiario += valorASerSomado;
+  let contadorTotal = dados.table[comandoContador.contadorTotal] || 0;
+  let contadorDiario =
+    dados.table.ultimaData === data
+      ? dados.table[comandoContador.contadorDiario] || 0
+      : 0;
 
-    client.say(
-        target,
-        `/me A @levxyca já disse ${comandoContador.palavra}! ${contadorDiario} vezes hoje e ${contadorTotal} vezes desde o dia ${DATA_INICIAL}.`,
-    );
+  const valorASerSomado =
+    comandoContador.temCombo && isComboValido(combo) ? parseInt(combo, 10) : 1;
+  contadorTotal += valorASerSomado;
+  contadorDiario += valorASerSomado;
 
-    dados.table[comandoContador.contadorTotal] = contadorTotal;
-    dados.table[comandoContador.contadorDiario] = contadorDiario;
-    dados.table.ultimaData = data;
+  client.say(
+    target,
+    `/me A @levxyca já disse ${comandoContador.palavra}! ${contadorDiario} vezes hoje e ${contadorTotal} vezes desde o dia ${DATA_INICIAL}.`,
+  );
 
-    salvarDados({...dados});
+  dados.table[comandoContador.contadorTotal] = contadorTotal;
+  dados.table[comandoContador.contadorDiario] = contadorDiario;
+  dados.table.ultimaData = data;
+
+  salvarDados({ ...dados });
 };
