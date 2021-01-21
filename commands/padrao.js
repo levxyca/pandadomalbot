b75d425ef3e98fa4818dc3691d5d15a8964028f5
kastrowalker <victorcsa2002@gmail.com>
2021-01-21 11:09:24 -0300

    Criando  addpoints

--- commands/padrao.js
+++ commands/padrao.js
@@ -2,9 +2,7 @@ const mensagens = [];
 const padroesFeitos = [];
 
 exports.default = (client, target, context, message) => {
-  console.log(message);
   mensagens.push(message);
-  console.log(padroesFeitos);
 
   if (
     mensagens[mensagens.length - 1] === mensagens[mensagens.length - 2] &&
