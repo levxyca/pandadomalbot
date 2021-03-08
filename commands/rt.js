23f5cea7f2efc7d0e2929a3ee842adee37e3a72c
Renan Gomes <renan.gomes@live.com>
2021-03-08 20:31:10 -0300

    Contagem de retweets/likes no comando !rt.

--- commands/rt.js
+++ commands/rt.js
@@ -7,9 +7,18 @@ exports.default = async (client, target, _, message) => {
   if (message.trim() === '!rt') {
     const tweet = await getTodaysLiveAnnouncement();
 
-    client.say(
-      target,
-      `Dá um RT aí por favorzinho levxycAnimada ${mountTweetUrl(tweet.id)}`,
-    );
+    let retweetMessage = '';
+    if (tweet.public_metrics.retweet_count > 0) {
+      retweetMessage += `🔁 ${tweet.public_metrics.retweet_count}. `;
+    }
+    if (tweet.public_metrics.like_count > 0) {
+      retweetMessage += `❤️ ${tweet.public_metrics.like_count}. `;
+    }
+
+    retweetMessage += `Dá um RT aí por favorzinho levxycAnimada ${mountTweetUrl(
+      tweet.id,
+    )}`;
+
+    client.say(target, retweetMessage);
   }
 };
