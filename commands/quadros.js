d1e8be4d56d86c17e6e456215864605fb980e97f
Leticia <leticiacaroline-123@hotmail.com>
2021-03-11 13:37:34 -0300

    :art: add quadros.js
    agrupando os comandos relacionados aos quadros do canal

---
+++ commands/quadros.js
@@ -0,0 +1,19 @@
+exports.default = (client, target, context, message) => {
+  switch (message) {
+    case '!pandaday':
+      client.say(
+        target,
+        `/me O pandaday é um dia especial onde o pandadomal pode tomar conta da live toda! Como funciona? A cada 1 sub, 300 bitus ou 10 reais de doação o pandadomal fica por 10 minutos na live. Esse tempo é cumulativo! O pandaday acontece toda sexta-feira.`,
+      );
+      break;
+    case '!levxoque':
+      client.say(
+        target,
+        `/me Levxoque ⚡ é um quadro do canal onde quebramos... digo, brincamos com hardware 
+          ⚙️ Isso não é um tutorial, a pessoa streamer não faz ideia do que está fazendo.`,
+      );
+      break;
+    default:
+      break;
+  }
+};
