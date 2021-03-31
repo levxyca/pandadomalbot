a17f70895ec269a4ff2364ef26cc1861b4325051
Leticia <leticiacaroline-123@hotmail.com>
2021-03-31 15:30:40 -0300

    :bug: fix bug of nicknames

--- commands/especiais.js
+++ commands/especiais.js
@@ -1,18 +1,26 @@
 exports.default = (client, target, context, message) => {
   switch (message) {
     case '!carrinho':
-      if (context.username === 'freakyfog') {
+      if (context.username.toLowerCase() === 'freakyfog') {
         client.say(target, `/me só me faz falta quem não vai na bola Kappa`);
       }
       break;
     case '!subgratis':
-      if (context.username === 'nesclimn10') {
+      if (context.username.toLowerCase() === 'nesclimn10') {
         client.say(target, `/me Dê um sub grátis pro nesclimn10.`);
       }
       break;
     case '!lanchinho':
-      if (context.username === 'PresidenteBolinho') {
+      if (context.username.toLowerCase() === 'presidentebolinho') {
         client.say(target, `/me É o momento perfeito para comer um bolinho.`);
+      }
+      break;
+    case '!subgratisedu':
+      if (context.username.toLowerCase() === 'eduardocodes') {
+        client.say(
+          target,
+          `/me EduardoCodes está querendo um gift sub. Quem se habilita?`,
+        );
       }
       break;
     default:
