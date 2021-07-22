a24af9981ce2c0db56a978f08360965d96f6e3fc
Leticia <leticiacaroline-123@hotmail.com>
2021-07-22 10:14:47 -0300

    :sparkles: add !addcarinho
    Comando que permite a pessoa streamer adicionar carinhos extras para as pessoas chatzeras.

--- commands/carinho/index.js
+++ commands/carinho/index.js
@@ -1,7 +1,9 @@
 const carinho = require('./commands/carinho');
 const estoque = require('./commands/estoque');
+const addcarinho = require('./commands/addcarinho');
 
-exports.default = async (client, target, context, message) => {
+exports.default = async (client, target, context, message, ehBot) => {
   carinho.default(client, target, context, message);
   estoque.default(client, target, context, message);
+  addcarinho.default(client, target, context, message, ehBot);
 };
