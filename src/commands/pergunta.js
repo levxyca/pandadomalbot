955ba96ae6d53ffbc601d43a71cd88f392311277
Leticia <leticiacaroline-123@hotmail.com>
2021-11-15 10:03:06 -0300

    add news answers

--- src/commands/pergunta.js
+++ src/commands/pergunta.js
@@ -1,18 +1,29 @@
 function getAnswer() {
   const answers = [
-    'sim.',
-    'não.',
-    'provavelmente.',
-    'com certeza!',
-    'hmmmmmm, será?',
-    'o que você acha?',
-    'acredite nos seus sonhos Kappa',
-    'vou ver e te aviso...',
-    'só tem um jeito de saber...',
-    'não conte com isso',
-    'é sério que você tá me perguntando isso?',
-    'eu gostaria de saber...',
-    'minha resposta vai mudar em algo?',
+    "sim.",
+    "não.",
+    "provavelmente.",
+    "com certeza!",
+    "hmmmmmm, será?",
+    "o que você acha?",
+    "acredite nos seus sonhos Kappa",
+    "vou ver e te aviso...",
+    "só tem um jeito de saber...",
+    "não conte com isso",
+    "é sério que você tá me perguntando isso?",
+    "eu gostaria de saber...",
+    "minha resposta vai mudar em algo?",
+    "fica o questionamento...",
+    "é uma questão deveras confusa...",
+    "impossível.",
+    "que pergunta, hein?",
+    "duvido muito.",
+    "não acredito nisso.",
+    "nem que o panda urre.",
+    "em breve.",
+    "tudo indica que sim",
+    "se concentra e pergunta de novo",
+    "é melhor eu não te contar agora",
   ];
 
   const answersCount = answers.length;
@@ -23,17 +34,17 @@ function getAnswer() {
 }
 
 exports.default = (client, target, context, message) => {
-  const command = message.split(' ');
+  const command = message.split(" ");
 
-  if (command[0] === '!pergunta' && command[1] === undefined) {
+  if (command[0] === "!pergunta" && command[1] === undefined) {
     client.say(
       target,
-      `/me Para utilizar o comando !pergunta você deve digitar !pergunta 'sua pergunta'`,
+      `/me Para utilizar o comando !pergunta você deve digitar !pergunta 'sua pergunta'`
     );
-  } else if (command[0] === '!pergunta') {
-    if (message.includes('9?')) {
+  } else if (command[0] === "!pergunta") {
+    if (message.includes("9?")) {
       client.say(target, `/me 9? 99? @${context.username}`);
-    } else if (message.includes('já pode?')) {
+    } else if (message.includes("já pode?")) {
       client.say(target, `/me Nunca pode @${context.username} Kappa`);
     } else {
       const answer = getAnswer();
