92c0ce0528ecbde2d87cce18c624fd7f38071b4f
Renan Gomes <renan.gomes@live.com>
2021-11-16 02:34:03 -0300

    Corrigindo carregamento de arquivos com underscore.

--- src/utilities/walk-commands.js
+++ src/utilities/walk-commands.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
+const path = require('path');
 
 const COMMANDS_PATH = `${__dirname}/../commands`;
 
-const getCommandFiles = (path) => {
+const getCommandFiles = (filePath) => {
   const files = [];
   // eslint-disable-next-line no-restricted-syntax
-  for (const file of fs.readdirSync(path)) {
-    if (!file.startsWith('_') || file.slice(-3) === '.js') {
-      const fullPath = `${path}/${file}`;
-      if (fs.lstatSync(fullPath).isDirectory())
-        getCommandFiles(fullPath).forEach((x) => files.push(`${file}/${x}`));
-      else files.push(file);
-    }
+  for (const file of fs.readdirSync(filePath)) {
+    const fullPath = `${filePath}/${file}`;
+    if (fs.lstatSync(fullPath).isDirectory())
+      getCommandFiles(fullPath).forEach((x) => files.push(`${file}/${x}`));
+    else files.push(file);
   }
   return files;
 };
@@ -21,9 +20,11 @@ function walkCommands() {
 
   // eslint-disable-next-line no-restricted-syntax
   for (const file of getCommandFiles(COMMANDS_PATH)) {
-    // eslint-disable-next-line import/no-dynamic-require,global-require
-    const module = require(`${COMMANDS_PATH}/${file}`);
-    commands.push(module);
+    if (!path.parse(file).base.startsWith('_') && file.slice(-3) === '.js') {
+      // eslint-disable-next-line import/no-dynamic-require,global-require
+      const module = require(`${COMMANDS_PATH}/${file}`);
+      commands.push(module);
+    }
   }
   return commands;
 }
