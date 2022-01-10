0b35d41c31715d1ed5b9c8f74d3e74786cc4d13f
levxyca <leticiacaroline-123@hotmail.com>
2022-01-10 13:24:55 -0300

    Adicionando comando !kotlinautas

---
+++ src/commands/misc/kotlinautas.js
@@ -0,0 +1,4 @@
+module.exports = {
+  keyword: 'kotlinautas',
+  execute: `Conheça a Kotlinautas, uma comunidade brasileira voltada pra Kotlin, buscando um ambiente amigável para iniciantes e inclusivo, Twitter => https://twitter.com/kotlinautas, Código de Conduta => https://github.com/Kotlinautas/codigo-de-conduta`,
+};
