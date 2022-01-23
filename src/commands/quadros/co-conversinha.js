eb59594505a9a49f1799c1e68d8ad3ffb2f93609
Leticia 'levxyca <leticiacaroline-123@hotmail.com>
2022-01-23 17:23:39 -0300

    add comandos para ver sobre os quadros do canal

---
+++ src/commands/quadros/co-conversinha.js
@@ -0,0 +1,5 @@
+module.exports = {
+    keyword: 'co-conversinha',
+    execute: `O co-conversinha é um quadro do canal onde temos o nosso co-working/co-studyng do jeitinho da 'levxyca' ⭐ Passe um tempo por aqui a converse comigo e com o chat enquanto fazemos algo diferente.`,
+  };
+  
---
+++ src/commands/quadros/logicadelevs.js
@@ -0,0 +1,5 @@
+module.exports = {
+    keyword: 'logicadelevs',
+    execute: `O logicadelevs 🤔 é um quadro do canal onde resolvemos problemas de lógica do URI com Javascript 💻 Lemos os problemas, rascunhamos o processamento dos dados e criamos o algoritmo 🔗 Confira o que já fizemos em: https://github.com/levxyca/uri`,
+  };
+  
