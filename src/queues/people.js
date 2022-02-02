375b4383b8f57fbdff231a3b2f83d266d64b513d
Renan Gomes <renan.gomes@live.com>
2022-02-02 09:51:23 -0300

    Classe para agrupar todos os atributos dos usuários.

--- src/queues/people.js
+++ src/queues/people.js
@@ -1,104 +1,46 @@
-/* eslint-disable no-param-reassign */
 const fastq = require('fastq');
+const Person = require('../models/person');
 const { read, write } = require('../utilities/data-file');
 
 /** Nome do arquivo a ser utilizado. */
 const FILE_NAME = 'people';
 
-// Pattern utilizado para pegar valores c/ sinal, ex: "valor", "+valor", "-valor"
-// eslint-disable-next-line prefer-regex-literals
-const ATTRIBUTE_UPDATE_REGEX = new RegExp('^(\\+|-)?(\\w*)');
+async function worker({ username, callback }) {
+  let content = read(FILE_NAME, []);
 
-function clearAttributes(attributes) {
-  Object.keys(attributes).forEach((key) => {
-    const match = ATTRIBUTE_UPDATE_REGEX.exec(attributes[key]);
-    if (match) {
-      const [, sign, value] = match;
-      if (!Number.isNaN(value)) {
-        attributes[key] = Number(value);
-        if (attributes[key] === 1 && sign === '-') attributes[key] = 0;
-      }
-    }
-  });
-  return attributes;
-}
+  let born = false;
+  let person = content.find((p) => p.name === username);
 
-function update(attributesToUpdate, newAttributes) {
-  const target = { ...attributesToUpdate };
-
-  Object.keys(target).forEach((key) => {
-    if (Object.prototype.hasOwnProperty.call(newAttributes, key)) {
-      const newValue = newAttributes[key];
-      const match = ATTRIBUTE_UPDATE_REGEX.exec(newValue);
-      if (match) {
-        const [, sign, value] = match;
-        switch (sign) {
-          case '+':
-            target[key] += Number(value);
-            break;
-          case '-':
-            target[key] -= Number(value);
-            if (target[key] < 0) {
-              target[key] = 0;
-            }
-            break;
-          default:
-            target[key] = value;
-        }
-      }
-      delete newAttributes[key];
-    }
-  });
-
-  Object.assign(target, clearAttributes(newAttributes));
-  return target;
-}
-
-async function worker({ username, attributes }) {
-  const content = read(FILE_NAME, {});
-  if (
-    attributes &&
-    Object.keys(attributes).length === 0 &&
-    Object.getPrototypeOf(attributes) === Object.prototype
-  ) {
-    return content[username];
-  }
-  if (!Object.prototype.hasOwnProperty.call(content, username)) {
-    content[username] = clearAttributes(attributes);
-    console.info(
-      `
-        Novo usuário criado: ${username}.
-        Atributos inicias: ${JSON.stringify(attributes)}.
-        `,
-    );
+  if (!person) {
+    born = true;
+    person = new Person(username);
   } else {
-    const oldAttributes = content[username];
-    const newAttributes = update(oldAttributes, attributes);
-
-    console.info(
-      `
-        Usuário ${username} atualizado.
-        Antes: ${JSON.stringify(oldAttributes)}.
-        Agora: ${JSON.stringify(newAttributes)}.
-        `,
-    );
-    content[username] = newAttributes;
+    person = Person.fromJSON(person);
   }
-  if (!write(FILE_NAME, content)) {
-    console.error('Falha ao salvar o arquivo');
+  const before = JSON.stringify(person);
+  const postExecution = callback(person);
+  const after = JSON.stringify(postExecution);
+
+  let log;
+  if (born) {
+    content = [...content, postExecution];
+    log = `[Novo usuário criado] :: Valores iniciais: ${after}`;
+  } else {
+    content = content.map((p) => (p.name === username ? postExecution : p));
+    log = `[Usuário modificado]\nAntes: ${before}\nAgora: ${after}`;
   }
 
-  return content[username];
+  console.info(log);
+  write(FILE_NAME, content);
 }
 
 const queue = fastq.promise(worker, 1);
 
-async function people(username, attributes = {}) {
-  const person = await queue.push({
+async function people(username, callback) {
+  await queue.push({
     username,
-    attributes,
+    callback,
   });
-  return person;
 }
 
 module.exports = { people };
