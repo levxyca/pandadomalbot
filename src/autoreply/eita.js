01a3df53f82fb7160a3b7eda4bcc2d469d511d61
omarkdev@gmail.com <omarkdev@gmail.com>
2022-02-09 10:27:18 -0300

    feat: Create autoreply to 'eita'
    Create autoreply to 'eita' message
    
    Closes #215

---
+++ src/autoreply/eita.js
@@ -0,0 +1,17 @@
+const patterns = [/^eita/i];
+
+const answer = () => `levxycEita levxycEita levxycEita`;
+
+/**
+ * Answer the "eita" message.
+ *
+ * @returns {string}
+ */
+function execute() {
+  return answer();
+}
+
+module.exports = {
+  patterns,
+  execute,
+};
