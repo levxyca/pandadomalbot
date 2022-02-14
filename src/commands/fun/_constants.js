94df04c87360404467512431d9ae0bf40d6f5bf6
levxyca <leticiacaroline-123@hotmail.com>
2022-02-14 10:48:32 -0300

    add mais levxycas

--- src/commands/fun/_constants.js
+++ src/commands/fun/_constants.js
@@ -1,3 +1,3 @@
-const levxycas = ['levestica', 'Levxícara', 'levchica'];
+const levxycas = ['levestica', 'Levxícara', 'levchica', 'levxuca', 'lévska', 'lev', 'levinha', 'delévs', 'Lév Francisca', 'Lév Chiquinha', 'levesticaixon', 'levscan', 'relampxyca', 'bicixyca', 'veiaxyca', 'roboxyca', 'levgyra', 'levxixica', 'leviajyca'];
 
 module.exports = { levxycas };
