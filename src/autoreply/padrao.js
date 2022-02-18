898d55c404d2a56cd75a70cb1f2f0a6e1ff49bef
Renan Gomes <renan.gomes@live.com>
2022-02-18 20:07:23 -0300

    Corrigindo padrão

--- src/autoreply/padrao.js
+++ src/autoreply/padrao.js
@@ -4,9 +4,13 @@ const REPLY_AFTER = 2; // Responder após quantas mensagens iguais.
 
 function execute({ message }) {
   messages.push(message);
-  if (messages.length === REPLY_AFTER && new Set(messages).size === 1) {
-    messages = [];
-    return message;
+
+  if (messages.length === REPLY_AFTER) {
+    if (new Set(messages).size === 1) {
+      messages = [];
+      return message;
+    }
+    messages.shift();
   }
   return null;
 }
