a9c081118c202a6e00116b6bfcffdb66d8554d26
levxyca <leticiacaroline-123@hotmail.com>
2022-03-10 14:12:03 -0300

    add !regras

---
+++ src/commands/misc/rules.js
@@ -0,0 +1,8 @@
+module.exports = {
+  keyword: 'regras',
+  execute: `◉ Por favor, nunca desrespeite ninguém: viewer, subscriber, mod ou streamer.
+            ◉ Apenas mande links relacionados ao que estamos estudando/conversando.
+            ◉ PROIBIDO mandar o código pronto de algo que a streamer está com dúvidas ou dificuldade, apenas se solicitado.
+            ◉ Quer mostrar outra maneira de desenvolver ou resolver algo? Tenha tenência no chat, você pode falar, por exemplo, "ei, depois se você tiver interesse, tem essa outra forma de fazer isso, aqui o link para a documentação".
+            ◉ Na dúvida, sempre pergunte antes para ter certeza de que está tudo bem!`,
+};
