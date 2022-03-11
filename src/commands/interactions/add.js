be456de3b478715ecc1bfb926801285d290e95e6
levxyca <leticiacaroline-123@hotmail.com>
2022-03-11 14:44:41 -0300

    migração do !add

---
+++ src/commands/interactions/add.js
@@ -0,0 +1,41 @@
+const { client } = require('../../core/twitch_client');
+const { people } = require('../../queues/people');
+
+module.exports = {
+  keyword: 'add',
+  async execute({ channel, context, argument }) {
+    if ('broadcaster' in context.badges) {
+      if (!argument) {
+        await client.say(
+          channel,
+          `Pô ${context.username}, eu preciso de mais informações para fazer isso.`,
+        );
+
+        return;
+      }
+      const infos = argument.split(' ');
+      const person = infos[0];
+      const qtd = infos[1];
+
+      if (Number.isNaN(qtd)) {
+        await client.say(
+          channel,
+          `Ei ${context.username}, adicione apenas números.`,
+        );
+
+        return;
+      }
+
+      await people(person, (p) => {
+        p.counter.irritations = Number(p.counter.irritations) + Number(qtd);
+        p.counter.affections = Number(p.counter.affections) + Number(qtd);
+        return p;
+      });
+    } else {
+      await client.say(
+        channel,
+        `${context.tusername} você não tem permissão para dar interações adicionais!`,
+      );
+    }
+  },
+};
