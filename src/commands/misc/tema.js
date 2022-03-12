40647c403250a603b3ba70e83824664375d801d2
levxyca <leticiacaroline-123@hotmail.com>
2022-03-11 21:31:38 -0300

    atualizando comando de tema

--- src/commands/misc/tema.js
+++ src/commands/misc/tema.js
@@ -2,5 +2,5 @@ module.exports = {
   keyword: 'tema',
   aliases: ['theme'],
   execute: `Esse é o tema usado no VSCode levxycAnimada
-  https://marketplace.visualstudio.com/items?itemName=jolaleye.horizon-theme-vscode`,
+  https://marketplace.visualstudio.com/items?itemName=alexandernanberg.horizon-theme-vscode`,
 };
