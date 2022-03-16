1b290da12b60fa4912e0fab2d371fd8551f1f668
Renan Gomes <renan.gomes@live.com>
2022-03-16 10:48:32 -0300

    Adicionando docs no arquivo da API

--- src/core/twitch_api.js
+++ src/core/twitch_api.js
@@ -9,6 +9,11 @@ class TwitchAPI {
     this.currentUser = null;
   }
 
+  /**
+   * Obtém os informações do usuário do canal.
+   *
+   * PS: execute o método TwitchAPI#fetchCurrentUser antes de acessar esta propriedade.
+   */
   get me() {
     return this.currentUser;
   }
@@ -47,6 +52,9 @@ class TwitchAPI {
     return true;
   }
 
+  /**
+   * Obtém os dados do usuário do canal.
+   */
   async fetchCurrentUser() {
     const user = await this.user(process.env.CHANNEL);
     if (user) {
@@ -58,6 +66,19 @@ class TwitchAPI {
    * Obtém os dados de um usuário pelo seu login.
    *
    * @param {String} username nome de usuário.
+   *
+   * @returns {
+   *    "id": "141981764",
+   *    "login": "twitchdev",
+   *    "display_name": "TwitchDev",
+   *    "type": "",
+   *    "broadcaster_type": "partner",
+   *    "description": "Description example.",
+   *    "profile_image_url": "https://image-example.png",
+   *    "offline_image_url": "https://image-example.pngg",
+   *    "view_count": 5980557,
+   *    "created_at": "2016-12-14T20:32:28Z"
+   * }
    */
   async user(username) {
     if (await this.#validate()) {
@@ -76,6 +97,21 @@ class TwitchAPI {
     throw new Error('Falha ao obter o usuário pela API.');
   }
 
+  /**
+   * Obtém as informações sobre follow entre dois usuários.
+   *
+   * @param {string} from ID do usuário interessado (ex: quem usou o comando).
+   * @param {string} to ID do usuário alvo (ex: dono do canal).
+   *
+   * @returns {
+   *    "from_id": "171003792",
+   *    "from_login": "iiisutha067iii"
+   *    "from_name": "IIIsutha067III"
+   *    "to_id": "23161357",
+   *    "to_name": "LIRIK",
+   *    "followed_at": "2017-08-22T22:55:24Z"
+   * }
+   */
   async follow(from, to) {
     if (await this.#validate()) {
       const response = await axios.get(
